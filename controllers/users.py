514d08fb139f6dcceaae19d545b459c54fff05af
kioopi <kioopi@7475d2c2-7ff4-11dd-aace-ddc5e87cd236>
2008-09-15 23:30:05 +0000

    Added gmap to user-profile 2

--- controllers/users.py
+++ controllers/users.py
@@ -11,8 +11,7 @@ class UserProfileController(webapp.RequestHandler):
         if user:
             users.create_logout_url(self.request.path)
             profile = UserProfile.gql("WHERE user = :1",  users.get_current_user()).get()
-            self.response.out.write(template.render(tmpl('templates/edit_profile.html'), {'user': user}))
-            # TODO: display profile form
+            self.response.out.write(template.render(tmpl('templates/edit_profile.html'), {'user': user, 'api_key': GOOGLE_MAPS_KEY}))
         else:
             users.create_login_url(self.request.path)
             self.response.out.write('<a href="%s">Login</a>' % users.create_login_url(self.request.uri))

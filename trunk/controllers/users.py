2b2220f5664491ccabb230065df53525403df9ea
kioopi <kioopi@7475d2c2-7ff4-11dd-aace-ddc5e87cd236>
2008-09-17 21:05:04 +0000

    user profile should be saved to db now.

--- trunk/controllers/users.py
+++ trunk/controllers/users.py
@@ -1,4 +1,4 @@
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp, db
 from models import UserProfile
 from settings import * 
 from google.appengine.api import users
@@ -11,9 +11,39 @@ class UserProfileController(webapp.RequestHandler):
         if user:
             #users.create_logout_url(self.request.path)
             profile = UserProfile.gql("WHERE user = :1",  users.get_current_user()).get()
-            self.response.out.write(template.render(tmpl('templates/edit_profile.html'), {'user': user, 'api_key': GOOGLE_MAPS_KEY}))
+            self.response.out.write(template.render(
+                tmpl('templates/edit_profile.html'), 
+                {'user': user, 'profile': profile, 'api_key': GOOGLE_MAPS_KEY}))
         else:
             #users.create_login_url(self.request.path)
             self.response.out.write('<a href="%s">Login</a>' % users.create_login_url(self.request.uri))
             # TODO: display read-only profile
 
+    def post(self):
+        """Recieves the Userprofile-form and updates the database. """
+        user = users.get_current_user()
+        # get the profile of the logged in user or create a new one
+        # there should be a method like get_or_create() but i cant find it 
+        # in the docs
+        profile = UserProfile.gql("WHERE user = :1",  users.get_current_user()).get()
+        if not profile: 
+            profile = UserProfile(user=user)
+      
+        # take the posted values and update the profile with it 
+        if self.request.get('mlurl',None): 
+            profile.movielens_url = db.Link(self.request.get('mlurl'))       
+
+        profile.coordinates  = db.GeoPt( 
+                           float(self.request.get('lat',0)),
+                           float(self.request.get('long',0)) )   
+        profile.put()
+ 
+        # the return to the same template as the get-controller 
+        # but pass a variable saved that tells the template that the
+        # profile was savegd
+        self.response.out.write(template.render(
+             tmpl('templates/edit_profile.html'),
+             {'user': user, 'profile': profile, 'saved':True, 'api_key': GOOGLE_MAPS_KEY})
+        )
+
+        

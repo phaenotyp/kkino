80af815e1bb5d6ad18cd1b2cfeefca1b7fae293c
kioopi <kioopi@7475d2c2-7ff4-11dd-aace-ddc5e87cd236>
2008-09-17 19:13:15 +0000

    refoctoring 2

--- controllers/users.py
+++ controllers/users.py
@@ -9,11 +9,11 @@ class UserProfileController(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            users.create_logout_url(self.request.path)
+            #users.create_logout_url(self.request.path)
             profile = UserProfile.gql("WHERE user = :1",  users.get_current_user()).get()
             self.response.out.write(template.render(tmpl('templates/edit_profile.html'), {'user': user, 'api_key': GOOGLE_MAPS_KEY}))
         else:
-            users.create_login_url(self.request.path)
+            #users.create_login_url(self.request.path)
             self.response.out.write('<a href="%s">Login</a>' % users.create_login_url(self.request.uri))
             # TODO: display read-only profile
 

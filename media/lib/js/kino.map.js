27b4fdeac236c72052eb9e85e34e154de01cbf91
phaenotyp <phaenotyp@7475d2c2-7ff4-11dd-aace-ddc5e87cd236>
2008-09-22 12:49:02 +0000

    kino uebersichtskarte

---
+++ media/lib/js/kino.map.js
@@ -0,0 +1,54 @@
+/**
+ * @author phaenotyp
+ */
+
+
+var kinoMaps = {
+	
+	initialize: function(){
+		
+		if (GBrowserIsCompatible()) {
+			
+			var map = new GMap2(document.getElementById("map"));
+			var markers = [];
+			map.setCenter(new GLatLng(50.940664, 6.959912), 13);
+			map.addControl(new GLargeMapControl());
+  			map.addControl(new GMapTypeControl());
+			
+			
+			
+			for (var i = 0; i < kinoGeo.kinos.length; i++) {
+			
+				if (kinoGeo.kinos[i].geo) {
+				
+					var kinoSpot = new GLatLng(kinoGeo.kinos[i].geo.lat, kinoGeo.kinos[i].geo.lng);
+					
+					
+					map.addOverlay(kinoMaps.createMarker(kinoSpot));
+
+				}
+			}
+			
+		}
+	},
+	
+	       // Creates a marker whose info window displays the letter corresponding
+        // to the given index.
+     createMarker:   function (point) {
+	 	
+
+          var marker = new GMarker(point);
+
+          GEvent.addListener(marker, "click", function() {
+            marker.openInfoWindowHtml("Marker <b></b>");
+          });
+          return marker;
+        }
+	
+}
+
+ 
+		  
+		  
+		  
+google.setOnLoadCallback(kinoMaps.initialize);		  

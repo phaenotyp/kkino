5223b2073fc65e4609f12b73cf22c3e11d898e88
phaenotyp <phaenotyp@7475d2c2-7ff4-11dd-aace-ddc5e87cd236>
2008-09-30 14:09:38 +0000

    Umnstellung auf Ajax-Api

--- trunk/api.py
+++ trunk/api.py
@@ -3,7 +3,7 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
 import datetime, cgi
-from import_models import Import
+#from import_models import Import
 from google.appengine.ext import db
 import util
 
